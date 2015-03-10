846e86485f535ba234e87a79e88064e5925bdba4
afranc15 <afranc15@asu.edu>
2015-03-09 17:56:46 -0700

    Start of the project

---
+++ JavaMD/src/Person.java
@@ -0,0 +1,22 @@
+
+public class Person {
+	
+	String gender;
+	int age;
+	
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
