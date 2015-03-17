59743a46e097e9ede48394ad3a10a24e2a55041a
afranc15 <afranc15@asu.edu>
2015-03-17 14:43:12 -0700

    Gui V1.002
    Able to switch to patient page with radio button correctly selected

--- JavaMD/src/JavaMDGui.java
+++ JavaMD/src/JavaMDGui.java
@@ -1,7 +1,10 @@
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+
 import java.awt.CardLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JInternalFrame;
 import javax.swing.JLayeredPane;
@@ -12,18 +15,26 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.BoxLayout;
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+
+import javax.swing.JPasswordField;
+
+import java.awt.Font;
 
 
 public class JavaMDGui {
 
 	private JFrame frmJavamd;
-	private JTextField txtPassword;
 	private JTextField txtUsername;
+	private JPasswordField pwdPassword;
+
 
 	/**
 	 * Launch the application.
@@ -45,6 +56,11 @@ public class JavaMDGui {
 	 * Create the application.
 	 */
 	public JavaMDGui() {
+		frmJavamd = new JFrame();
+		frmJavamd.setTitle("JavaMD");
+		frmJavamd.setBounds(100, 100, 450, 300);
+		frmJavamd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmJavamd.getContentPane().setLayout(new CardLayout(0, 0));
 		initialize();
 	}
 
@@ -52,11 +68,6 @@ public class JavaMDGui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmJavamd = new JFrame();
-		frmJavamd.setTitle("JavaMD");
-		frmJavamd.setBounds(100, 100, 450, 300);
-		frmJavamd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmJavamd.getContentPane().setLayout(new CardLayout(0, 0));
 		
 		JPanel panelLogin = new JPanel();
 		frmJavamd.getContentPane().add(panelLogin, "name_31186421828104");
@@ -67,23 +78,8 @@ public class JavaMDGui {
 		panelLogin.add(rdbtnDoctor);
 		
 		JRadioButton rdbtnPatient = new JRadioButton("Patient");
-		rdbtnPatient.setBounds(209, 72, 75, 23);
+		rdbtnPatient.setBounds(202, 72, 75, 23);
 		panelLogin.add(rdbtnPatient);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		btnLogin.setBounds(270, 148, 117, 29);
-		panelLogin.add(btnLogin);
-		
-		txtPassword = new JTextField();
-		txtPassword.setText("Password");
-		txtPassword.setBounds(134, 147, 134, 28);
-		panelLogin.add(txtPassword);
-		txtPassword.setColumns(10);
 		
 		txtUsername = new JTextField();
 		txtUsername.setText("Username");
@@ -91,18 +87,59 @@ public class JavaMDGui {
 		panelLogin.add(txtUsername);
 		txtUsername.setColumns(10);
 		
-		JPanel panelPatientMenu = new JPanel();
-		frmJavamd.getContentPane().add(panelPatientMenu, "name_31385619939746");
+		pwdPassword = new JPasswordField();
+		pwdPassword.setText("Password");
+		pwdPassword.setBounds(134, 147, 134, 28);
+		panelLogin.add(pwdPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(rdbtnPatient.isSelected() == true && rdbtnDoctor.isSelected() == false){
+					panelLogin.setVisible(false);
+					intializePatientMenu();
+				}else{
+					
+				}
+			}
+		});
+		btnLogin.setBounds(270, 148, 117, 29);
+		panelLogin.add(btnLogin);
+		
 		
 		JPanel panelPatientCreateForm = new JPanel();
 		frmJavamd.getContentPane().add(panelPatientCreateForm, "name_31828079017857");
 		panelPatientCreateForm.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		
-		
+	}
 	
+	private void intializePatientMenu(){
 		
+		JPanel panelPatientMenu = new JPanel();
+		frmJavamd.getContentPane().add(panelPatientMenu, "name_31385619939746");
+		panelPatientMenu.setLayout(null);
 		
+		JButton btnCreateForm = new JButton("Create Form");
+		btnCreateForm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnCreateForm.setBounds(28, 63, 117, 29);
+		panelPatientMenu.add(btnCreateForm);
 		
+		JButton btnCheckForms = new JButton("Check Forms");
+		btnCheckForms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCheckForms.setBounds(28, 122, 117, 29);
+		panelPatientMenu.add(btnCheckForms);
+		
+		JLabel lblHelloAdam = new JLabel("Hello, Adam");
+		lblHelloAdam.setFont(new Font("Lucida Grande", Font.PLAIN, 22));
+		lblHelloAdam.setBounds(100, 5, 175, 46);
+		panelPatientMenu.add(lblHelloAdam);
+		
+		panelPatientMenu.setVisible(true);
 	}
 }
