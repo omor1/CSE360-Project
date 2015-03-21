04e68e644de5b6d67c77d2bd48c742363468e4fc
omor1 <omri50@gmail.com>
2015-03-20 17:18:32 -0700

    src/Person.java, src/Doctor.java: formatting fixes

--- JavaMD/src/Doctor.java
+++ JavaMD/src/Doctor.java
@@ -2,16 +2,14 @@ import java.util.UUID;
 
 import javax.mail.internet.InternetAddress;
 
-
 public class Doctor extends Person {
-	
+
 	private int threshold;
-	
+
 	public Doctor(String name, UUID id, InternetAddress email) {
 		super(name, id, email);
 		this.setThreshold(0);
 	}
-
 
 	public int getThreshold() {
 		return threshold;
@@ -20,5 +18,5 @@ public class Doctor extends Person {
 	public void setThreshold(int threshold) {
 		this.threshold = threshold;
 	}
-	
+
 }
