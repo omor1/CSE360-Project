76a4af22b9b7abd8300b600ceb8678ccbb8fb268
afranc15 <afranc15@asu.edu>
2015-03-22 12:06:29 -0700

    New Gui
    Username : user
    Password : 1234

---
+++ JavaMD2/src/Person.java
@@ -0,0 +1,32 @@
+import java.util.UUID;
+import javax.mail.internet.InternetAddress;
+
+public class Person {
+
+	private String name;
+	private final UUID id;
+	private final InternetAddress email;
+
+	public Person(String name, UUID id, InternetAddress email) {
+		this.setName(name);
+		this.id = id;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public InternetAddress getEmail() {
+		return email;
+	}
+
+}
