d3f5a78334a17c991c6a34a01e9ac0474ee9c5a4
omor1 <omri50@gmail.com>
2015-04-08 18:39:36 -0700

    JavaMD2/src/MySQLConnect.java: throws ClassNotFoundException, SQLException JavaMD2/src/MySQLConnect.java: formatting

--- JavaMD2/src/MySQLConnect.java
+++ JavaMD2/src/MySQLConnect.java
@@ -1,16 +1,11 @@
 import java.sql.*;
-import javax.swing.*;
+
 public class MySQLConnect {
 	Connection conn = null;
-	public static Connection ConnectDb(){
-		try{
+	public static Connection ConnectDb() throws ClassNotFoundException, SQLException {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://Localhost/student","root","root");
 			System.out.println("Connection work!");
 			return conn;
-		}catch(Exception e){
-			JOptionPane.showMessageDialog(null,e);
-			return null;
-		}
 	}
 }
