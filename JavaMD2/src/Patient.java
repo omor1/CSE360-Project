ef749a2cbe3abcf8e2946185a8515fbedcede555
omor1 <omri50@gmail.com>
2015-04-17 18:13:24 -0700

    bugfixes for patient.java

--- JavaMD2/src/Patient.java
+++ JavaMD2/src/Patient.java
@@ -11,42 +11,42 @@ public class Patient extends Person {
 		MALE, FEMALE, OTHER
 	}
 
-	public class MedicalInformation {
-
-		public class Prescription {
-			private final String name;
-			private final double amount;
-
-			public Prescription(String name, double amount) {
-				this.name = name;
-				this.amount = amount;
-			}
-
-			public String getName() {
-				return name;
-			}
-
-			public double getAmount() {
-				return amount;
-			}
-		}
-
-		private final List<String> allergies;
-		private final List<Prescription> prescriptions;
-
-		public MedicalInformation() {
-			this.allergies = new LinkedList<>();
-			this.prescriptions = new LinkedList<>();
-		}
-
-		public List<String> getAllergies() {
-			return allergies;
-		}
-
-		public List<Prescription> getPrescriptions() {
-			return prescriptions;
-		}
-	}
+//	public class MedicalInformation {
+//
+//		public class Prescription {
+//			private final String name;
+//			private final double amount;
+//
+//			public Prescription(String name, double amount) {
+//				this.name = name;
+//				this.amount = amount;
+//			}
+//
+//			public String getName() {
+//				return name;
+//			}
+//
+//			public double getAmount() {
+//				return amount;
+//			}
+//		}
+//
+//		private final List<String> allergies;
+//		private final List<Prescription> prescriptions;
+//
+//		public MedicalInformation() {
+//			this.allergies = new LinkedList<>();
+//			this.prescriptions = new LinkedList<>();
+//		}
+//
+//		public List<String> getAllergies() {
+//			return allergies;
+//		}
+//
+//		public List<Prescription> getPrescriptions() {
+//			return prescriptions;
+//		}
+//	}
 
 	private Gender gender;
 	//private final LocalDate birthDate;
@@ -75,36 +75,36 @@ public class Patient extends Person {
 		this.gender = gender;
 	}
 
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public double getHeight() {
-		return height;
-	}
-
-	public void setHeight(double height) {
-		this.height = height;
-	}
-
-	public double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-
-	public MedicalInformation getMedicalInformation() {
-		return medicalInformation;
-	}
-
-	public List<Form> getForms() {
-		return forms;
-	}
-
-	public Period getAge() {
-		return Period.between(this.getBirthDate(), LocalDate.now());
-	}
+//	public LocalDate getBirthDate() {
+//		return birthDate;
+//	}
+//
+//	public double getHeight() {
+//		return height;
+//	}
+//
+//	public void setHeight(double height) {
+//		this.height = height;
+//	}
+//
+//	public double getWeight() {
+//		return weight;
+//	}
+//
+//	public void setWeight(double weight) {
+//		this.weight = weight;
+//	}
+//
+//	public MedicalInformation getMedicalInformation() {
+//		return medicalInformation;
+//	}
+//
+//	public List<Form> getForms() {
+//		return forms;
+//	}
+//
+//	public Period getAge() {
+//		return Period.between(this.getBirthDate(), LocalDate.now());
+//	}
 
 }
